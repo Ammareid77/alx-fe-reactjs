96970dee640e9e5f2053fdc092efed095457761f
Ammar Eid <ammareidd7@gmail.com>
2025-03-02 22:44:11 +0200

    The second commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,3 +1,5 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {RecipeList} from './components/RecipeList';
 import {AddRecipeForm} from './components/AddRecipeForm';
