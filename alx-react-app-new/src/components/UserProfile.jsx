eb4320e58c7ae86ebdcc42832533cfa8ca3af8ea
Ammar Eid <ammareidd7@gmail.com>
2025-03-02 22:37:08 +0200

    The second commit

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,6 +2,8 @@
 function UserProfile(props){
     return (
         <div>
+            <span style={{ color: 'blue', border: 'solid 10px', padding: '10px', margin: '10px' }}>
+            </span>
             <h2>{props.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
