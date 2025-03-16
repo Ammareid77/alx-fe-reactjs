814f9671965d4ef2c0a58b537aa15bbaf9488bbf
Ammar Eid <ammareidd7@gmail.com>
2025-03-17 00:52:53 +0200

    Delete react-todo/jest.config.js

--- react-todo/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  testMatch: ["**/__tests__/**/*.test.js", "**/?(*.)+(spec|test).[tj]s?(x)"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest"
-  }
-};
