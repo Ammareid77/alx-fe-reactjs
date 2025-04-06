fddd52745cc86a263e6bc829a0cdf59626f78f26
Ammar Eid <Ammarkamall777@gmail.com>
2025-04-06 22:32:49 +0200

    Fix path

--- react-todo/src/TodoList.jsx
+++ react-todo/src/TodoList.jsx
@@ -1,54 +1,54 @@
 import React, { useState } from "react";
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([
+const [todos, setTodos] = useState([
     { id: 1, text: "Sample Todo", completed: false },
-  ]);
-  const [input, setInput] = useState("");
+]);
+const [input, setInput] = useState("");
 
-  const addTodo = (e) => {
+const addTodo = (e) => {
     e.preventDefault();
     if (input.trim()) {
-      setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
-      setInput("");
+    setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+    setInput("");
     }
-  };
+};
 
-  const toggleTodo = (id) => {
+    const toggleTodo = (id) => {
     setTodos(todos.map(todo =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    todo.id === id ? { ...todo, completed: !todo.completed } : todo
     ));
-  };
+};
 
-  const deleteTodo = (id) => {
+    const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
-  };
+};
 
-  return (
+    return (
     <div>
-      <form onSubmit={addTodo}>
+    <form onSubmit={addTodo}>
         <input
-          placeholder="Add todo"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
+        placeholder="Add todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         />
         <button type="submit">Add</button>
-      </form>
+    </form>
 
-      <ul>
+    <ul>
         {todos.map((todo) => (
-          <li
+        <li
             key={todo.id}
             onClick={() => toggleTodo(todo.id)}
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-          >
+        >
             {todo.text}
             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
+        </li>
         ))}
-      </ul>
+    /ul>
     </div>
-  );
+);
 };
 
 export default TodoList;
